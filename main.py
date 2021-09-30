6892dc8b8f43ca6a93aff0e699861c45c98aedec
anuran <anuranroy02@gmail.com>
2021-10-01 01:14:35 +0530

    Initial commit: Basic skeleton

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import modules.hashing as h
 from typing import  Optional, List
 from sqlalchemy.orm import  Session
 from database.db import engine, SessionLocal
+
+app = FastAPI()
+
+@app.get('/')
+async def index():
+    return {"details": "Hello World"}
